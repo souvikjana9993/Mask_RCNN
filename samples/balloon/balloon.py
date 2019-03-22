1237ae1d72bf0225fae01592d1ecb814379b201c
souvikjana1993 <souvikjana1993@gmail.com>
2019-03-22 18:48:00 +0530

    Update balloon.py

--- samples/balloon/balloon.py
+++ samples/balloon/balloon.py
@@ -249,6 +249,36 @@ def color_splash(image, mask):
     return splash
 
 
+def display_instances(image, boxes, masks, ids, names, scores):
+    """
+        take the image and results and apply the mask, box, and Label
+    """
+    n_instances = boxes.shape[0]
+    colors = random_colors(n_instances)
+
+    if not n_instances:
+        print('NO INSTANCES TO DISPLAY')
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == ids.shape[0]
+
+    for i, color in enumerate(colors):
+        if not np.any(boxes[i]):
+            continue
+
+        y1, x1, y2, x2 = boxes[i]
+        label = names[ids[i]]
+        score = scores[i] if scores is not None else None
+        caption = '{} {:.2f}'.format(label, score) if score else label
+        mask = masks[:, :, i]
+
+        image = apply_mask(image, mask, color)
+        image = cv2.rectangle(image, (x1, y1), (x2, y2), color, 2)
+        image = cv2.putText(
+            image, caption, (x1, y1), cv2.FONT_HERSHEY_COMPLEX, 0.7, color, 2
+        )
+
+    return image
+
 def detect_and_color_splash(model, image_path=None, video_path=None):
     assert image_path or video_path
 
