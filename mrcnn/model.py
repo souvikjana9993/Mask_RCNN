f39e7f8feafc6b7fc150481cc0303f2da720fdb8
souvikjana1993 <souvikjana1993@gmail.com>
2019-03-24 12:22:11 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2140,15 +2140,23 @@ class MaskRCNN():
         """Downloads ImageNet trained weights from Keras.
         Returns path to weights file.
         """
-        from keras.utils.data_utils import get_file
-        TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/'\
-                                 'releases/download/v0.2/'\
-                                 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
-        weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+	from keras.utils.data_utils import get_file
+	TF_WEIGHTS_PATH_NO_TOP = 'https://drive.google.com/uc?export=download&confirm=XhRM&id=1-0MCni9Ur9p58xdAhkTtP-JU4nOQwM3u'
+        weights_path = get_file('custom_resnet_weights.h5',
                                 TF_WEIGHTS_PATH_NO_TOP,
                                 cache_subdir='models',
                                 md5_hash='a268eb855778b3df3c7506639542a6af')
         return weights_path
+	
+# 	from keras.utils.data_utils import get_file
+#         TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/'\
+#                                  'releases/download/v0.2/'\
+#                                  'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+#         weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+#                                 TF_WEIGHTS_PATH_NO_TOP,
+#                                 cache_subdir='models',
+#                                 md5_hash='a268eb855778b3df3c7506639542a6af')
+#         return weights_path
 
     def compile(self, learning_rate, momentum):
         """Gets the model ready for training. Adds losses, regularization, and
